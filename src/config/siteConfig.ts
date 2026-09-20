d91fab92feea85e6dbc958738f1e68be2914420c
ngu-ki <nguki@nguki.com>
2026-09-20 13:48:18 +0800

    更新

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -112,8 +112,8 @@ export const siteConfig: SiteConfig = {
 		// 使用 Astro 图标库时不需要设置 valueDark，图标会自动跟随主题亮暗色切换
 		logo: {
 			type: "image",
-			value: "assets/images/logo/maw-light.svg",
-			valueDark: "assets/images/logo/maw-dark.svg",
+			value: "/assets/images/logo/maw-light.svg",
+			valueDark: "/assets/images/logo/maw-dark.svg",
 			alt: "🍀",
 		},
 		// 导航栏标题
